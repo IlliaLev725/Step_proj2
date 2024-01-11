ea0708b2e52eaae1ff7296372c3f36bcb01c58ef
Illia Levchenko <111638480+IlliaLev725@users.noreply.github.com>
2024-01-11 18:18:26 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const port = 80;
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
-  res.status(404).send('Hello World!');
+  //res.status(404).send('Hello World!');
 });
 
 var server = app.listen(port, () => {
